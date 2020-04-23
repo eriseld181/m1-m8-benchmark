5b8ba9947abd75541686f9d1f48ce7c5e038aef8
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-04-23 16:54:59 +0200

    update23

---
+++ day 8/test.js
@@ -0,0 +1,12 @@
+var totalBingoNumbers = 4;
+var choose =  [1]
+var randNum= []
+for (let i = 0; i < totalBingoNumbers; i++) {
+    randNum= [i]
+   
+        console.log("congratulations you won the bingo",randNum)
+    
+
+    }
+    
+
