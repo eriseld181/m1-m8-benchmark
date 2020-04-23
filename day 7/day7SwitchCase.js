506eefccd9791046dde727228500f6a627bd46ba
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-04-23 08:40:21 +0200

    day 22, uodate(late update)

---
+++ day 7/day7SwitchCase.js
@@ -0,0 +1,19 @@
+/*switch case*/
+const x = "value2";
+switch (x){
+    case "value1":
+        console.log("value 1 branch");
+        //
+        break;
+        case "value2":
+            console.log("value 2 branch");
+            //
+            break;
+            case "value3":
+                console.log("value 3 branch");
+                //
+                break;
+
+            default: 
+            console.log("none of the previous");
+}
