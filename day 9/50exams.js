7cc8ec51f46ce3b73241a2b62d9c7f426cd5e5b7
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-04-24 16:52:00 +0200

    last update

--- day 9/50exams.js
+++ day 9/50exams.js
@@ -67,35 +67,29 @@ console.log(myArrayOfArrays)
         let ArraysumOfmyArray3 = myArray3.reduce(function(a, b){
             return a + b;
         }, 0);
-        if(ArraysumOfmyArray3 > ArraysumOfmyArray4){
-            console.log("the biggest sum is array4", ArraysumOfmyArray4)
+          if(ArraysumOfmyArray3 > ArraysumOfmyArray4){
+            return  console.log("the biggest sum is array4", ArraysumOfmyArray4)
         }else{
-            console.log( ArraysumOfmyArray3 )
-        }
+            return  console.log( ArraysumOfmyArray3 )
+        }  
         
     }
+   
+
+
     
 /*JS EXERCISES
 
        
-        22) Create an object with properties such Name, Surname, Email
-        23) Delete Email from the previously created object
-        24) Create an array with 10 strings in it
-        25) Print in the console every string in the previous array
-       - 26) Create an array with 100 random numbers in it
-       - 27) Write a function to get the MAX and the MIN from the previously created array
-        -28) Create an array of arrays, in which every array has 10 random numbers
-        -29) Create a function that gets 2 arrays and returns the longest one
-        -30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
-
+    
         DOM 
 
-        31) Get element with ID "container" from the page
-        32) Get every "td" from the page
-        33) Create a cycle that prints the text inside every td of the page
-        34) Write a function to change the heading of the page
-        35) Write a function to add an extra row to the table
-        36) Write a function to add the class "test" to reach row in the table
+        -31) Get element with ID "container" from the page
+       - 32) Get every "td" from the page
+       - 33) Create a cycle that prints the text inside every td of the page
+       - 34) Write a function to change the heading of the page
+        -35) Write a function to add an extra row to the table
+       - 36) Write a function to add the class "test" to reach row in the table
         37) Write a function to add a red background to every link in the page
         38) Console log "Page loaded" when the page is correctly loaded
         39) Write a function to add new items to a UL
